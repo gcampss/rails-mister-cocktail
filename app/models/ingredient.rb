f969e9e783e8e3ca1266646a7aba671b57ad0f78
Gabriel Campista <gcampss@gmail.com>
2020-11-07 12:17:45 +0000

    add stuff for cloudnary

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,8 @@
 class Ingredient < ApplicationRecord
-    has_many :doses #, dependent: :restrict_with_error
+  has_many :doses #, dependent: :restrict_with_error
 
-    validates :name, presence: true, uniqueness: true
-    scope :ordered, -> { order(name: :asc) }
+  validates :name, presence: true, uniqueness: true
+  scope :ordered, -> { order(name: :asc) }
+  has_one_attached :photo
 end
+
